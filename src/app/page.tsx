6400a0c2a54be246b3437e26b3c57edd310c0669
AndreQuintero <andre.quintero96@gmail.com>
2025-01-02 01:01:00 +0800

    add: twitter form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-
 import { Hero } from './components/hero';
+import { TwitterForm } from './components/twitter-form';
 import { Container } from './ui/container';
+
 
 export default function Home() {
   return (
     <Container>
      <Hero />
+     <TwitterForm />
     </Container>
     
   );
