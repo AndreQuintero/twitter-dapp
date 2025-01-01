e8b4bfda125518dc12f3bb2a405deba9e2e2c18e
AndreQuintero <andre.quintero96@gmail.com>
2025-01-02 01:00:21 +0800

    add: width 100%

--- src/app/layout/tweet-post/tweet-post.tsx
+++ src/app/layout/tweet-post/tweet-post.tsx
@@ -13,7 +13,7 @@ export const TweetPost = ({ characterLimit = 280 }:TweetPostProps) => {
      const [message, setMessage] = useState('');
 
     return (
-        <div className={css({mt: '1.6rem'})}>
+        <div className={css({mt: '1.6rem', width: '100%'})}>
             <TextArea maxLength={characterLimit} onChange={(e) => setMessage(e.target.value)} placeholder="What's on your mind?" ref={textRef}/>
             <p className={css({textAlign: 'right'})}>you can still type more: <strong>{characterLimit - message.length }</strong></p>
             <Button onClick={() => console.log(textRef.current?.getValue())}>Post</Button>
