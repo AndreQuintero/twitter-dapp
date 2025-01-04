9b6a7b10078fc0919fbeb8b65b1b94e110047ec8
AndreQuintero <andre.quintero96@gmail.com>
2025-01-04 23:49:57 +0800

    add: exporting useAccount hook type

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -2,4 +2,8 @@ import { useAccount } from './useAccount'
 import { useContract } from './useContract'
 import { useProfile } from './useProfile'
 
+type UseAccount = typeof useAccount
+
+
 export { useAccount, useContract, useProfile }
+export type { UseAccount }
