02760d9edd6fbe104cb2b63d1631c8850ccb89e4
AndreQuintero <andre.quintero96@gmail.com>
2025-01-04 23:49:16 +0800

    add: conditional viewing for the components

--- src/app/components/twitter-form/twitter-form.tsx
+++ src/app/components/twitter-form/twitter-form.tsx
@@ -1,12 +1,27 @@
+"use client"
 import { Account } from "@/app/layout/account"
 import { TweetPost } from "@/app/layout/tweet-post"
 import { vstack } from "../../../../styled-system/patterns"
+import { useAccount, useProfile } from "@/app/hooks"
+import { useStore } from "@/app/store"
+
 
 export const TwitterForm = () => {
+    const { isConnected } = useAccount(useStore)
+    const {profileExists} = useProfile(useStore, useAccount)
+    
+
+    const showProfileForm = () => {
+        if(isConnected() && !profileExists()) {
+            return true
+        }
+        return false
+    }
+
     return (
         <div className={vstack()}>
-            <Account />
-            <TweetPost />
+            {showProfileForm() && <Account />}
+            {profileExists() && <TweetPost />}
         </div>
     )
 }
