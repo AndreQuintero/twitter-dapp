96799fb40c7e512b430318908f65ce0c2d476626
AndreQuintero <andre.quintero96@gmail.com>
2025-01-04 23:52:23 +0800

    add: profile store in general store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,10 +1,14 @@
 import { create } from "zustand";
 import { Store } from "../types/store";
 import { createAccountSlice } from "./account-slice"
+import { createProfileSlice, ProfileState } from './profile-slice'
 import { immer } from "zustand/middleware/immer";
 
 export const useStore = create<Store>()(immer((...a) => ({
-    ...createAccountSlice(...a)
+    ...createAccountSlice(...a),
+    ...createProfileSlice(...a)
 })))
 
-export type UseStoreType = typeof useStore
+type UseStoreType = typeof useStore
+
+export type {UseStoreType, ProfileState}
--- src/app/types/store.ts
+++ src/app/types/store.ts
@@ -1,3 +1,4 @@
 import { AccountSlice } from "../store/account-slice";
+import { ProfileSlice } from "../store/profile-slice";
 
-export type Store = AccountSlice
+export type Store = AccountSlice & ProfileSlice
