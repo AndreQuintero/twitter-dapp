bb831a7c6fa99632bf04e05b586771e373900ade
AndreQuintero <andre.quintero96@gmail.com>
2025-01-06 01:18:58 +0800

    add: using post tweet method

--- src/app/layout/tweet-post/tweet-post.tsx
+++ src/app/layout/tweet-post/tweet-post.tsx
@@ -3,19 +3,28 @@ import { Button } from "@/app/ui/button"
 import { TextArea, TextAreaRefProps } from "@/app/ui/textarea"
 import {  useRef, useState } from "react"
 import { css } from "../../../../styled-system/css"
+import { useAccount, useTweet } from "@/app/hooks"
+import { useStore } from "@/app/store"
 
 type TweetPostProps = {
     characterLimit?: number
 }
 
 export const TweetPost = ({ characterLimit = 280 }:TweetPostProps) => {
-     const textRef = useRef<TextAreaRefProps>(null)
-     const [message, setMessage] = useState('')
+    const textRef = useRef<TextAreaRefProps>(null)
+    const { postTweets: _postTweets } = useTweet(useStore, useAccount)
+    const [message, setMessage] = useState('')
+
+    const postTweets = async () => {
+        await _postTweets(textRef.current?.getValue() ?? '')
+        textRef.current?.reset()
+    }
+
     return (
         <div className={css({mt: '1.6rem', width: '100%'})}>
             <TextArea maxLength={characterLimit} onChange={(e) => setMessage(e.target.value)} placeholder="What's on your mind?" ref={textRef}/>
             <p className={css({textAlign: 'right'})}>you can still type more: <strong>{characterLimit - message.length }</strong></p>
-            <Button onClick={() => console.log(textRef.current?.getValue())}>Post</Button>
+            <Button onClick={postTweets}>Post</Button>
         </div>
     )
 }
