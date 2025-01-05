14e711874db6a08805a1f3e92ec41b75cc73c231
AndreQuintero <andre.quintero96@gmail.com>
2025-01-06 01:40:13 +0800

    change: removing export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-export const poppinsFont = Poppins({
+const poppinsFont = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
   weight: ["100","200","300","400","500", "600", "700", "800"]
